f8542d10772b89dfaadcf668bf43b94b2ae06225
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2022-01-27 14:55:52 +1100

    Document AllSettledPromise

--- src/main/java/me/sparky/promises/AllSettledPromise.java
+++ src/main/java/me/sparky/promises/AllSettledPromise.java
@@ -23,11 +23,24 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * A promise that resolves to a list of each input promise's state when they are all settled (completed).
+ *
+ * @author Sparky
+ * @since 1.2
+ */
 public class AllSettledPromise extends AbstractCompletablePromise<List<Promise.State>> {
     
     private final List<State> outcomes;
     private int i = 0;
     
+    /**
+     * Constructs an <code>AllSettledPromise</code> with specified promises.
+     *
+     * @param promises The promises
+     * @throws IllegalArgumentException if promises is null
+     * @since 1.2
+     */
     public AllSettledPromise(Collection<Promise<?>> promises) {
         
         this.outcomes = new ArrayList<>(Collections.nCopies(promises.size(), null));
