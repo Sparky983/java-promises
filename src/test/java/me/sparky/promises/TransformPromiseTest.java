39663c0cb49c0128dfcd67df1e04184a1eb5f371
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2022-01-28 14:09:58 +1100

    Optimize imports

--- src/test/java/me/sparky/promises/TransformPromiseTest.java
+++ src/test/java/me/sparky/promises/TransformPromiseTest.java
@@ -19,7 +19,8 @@ package me.sparky.promises;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class TransformPromiseTest {
 
